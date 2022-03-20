9301ca4adb66e7291a35afa310435cc51fef8189
Marco Lanfranchi <>
2022-03-20 21:12:11 +0100

    Introduzione mockData

---
+++ src/assets/mockData.ts
@@ -0,0 +1,118 @@
+import { CardInfo } from "../components/model/rest";
+
+const cards: CardInfo[] = [
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+  {
+    name: "Londra",
+    from: new Date("25 Mar 2022"),
+    to: new Date("28 Mar 2022"),
+    description:
+      "Viaggio a Lndn. Parto per tre giorni a Londra con Alice. Poca spesa tanta resa e speriamo di non trovare brutto tempo.",
+    budget: 800,
+  },
+];
+
+export { cards };
